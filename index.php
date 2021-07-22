ad5ef91c4f27d58baaf94bef109fae55d5562283
Bojerland <85882819+Bojerland@users.noreply.github.com>
2021-07-22 19:00:02 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>basico PHP</title>
+</head>
+<body>
+
+	<h2>Mi primer programa en PHP </h2>
+
+	<?php
+
+	// declaro las variables
+	$nombre = "Cecilia";
+	$edad = 49;
+	$hobbie = "huerta";
+	$editorDeCodigo = "Sublime Text";
+	$sistOp = "CentOS8";
+
+	# imprimo un texto en pantalla
+	echo "<span>Nombre: </span>".$nombre."<br>" ;
+	echo "<span>Edad: </span>".$edad." años<br>" ;
+	echo "<span>Hobbie: </span>".$hobbie."<br>" ;
+	echo "<span>Editor de código favorito: </span>".$editorDeCodigo."<br>" ;
+	echo "<span>Sistema Operativo que usa: </span>".$sistOp."<br>" ;
+	?>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+ 
